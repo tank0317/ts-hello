afe9f6c9e06de32cc0e92587997788d0b86991ce
tank0317 <fengweiyao0317@hotmail.com>
2019-12-19 16:17:36 +0800

    add src and eslint

---
+++ src/hello.ts
@@ -0,0 +1,19 @@
+export const enum Direction {
+  UP,
+  DOWN
+}
+
+export const enum Probe {
+  Default,
+  Throttle,
+  Normal,
+  Realtime
+}
+
+export class BScroll {
+  enable() {}
+}
+
+export class Options {
+  process() {}
+}
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import { BScroll, Direction } from "./hello";
+
+export const up = Direction.UP
+export const bscroll: BScroll = new BScroll()
